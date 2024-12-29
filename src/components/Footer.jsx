bb94b099f6285ebf769aecd31d7986d65514f6fd
Dawit Belay <dawitbelay27@gmail.com>
2024-12-29 02:32:37 -0800

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+          <p className="text-gray-600">© 2024 D_SHOP. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
